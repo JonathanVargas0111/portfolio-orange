a0318f9dcfdd3ce89cdd5b41026038c99ce2f5ce
felipe <jonathanfelipe0111@hotmail.com>
2024-08-28 19:59:20 -0500

    fix: correct padding top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,8 +43,8 @@ const Navbar = () => {
 
     return (
 
-        <div className='text-white/70 pt-6'>
-            <div className="hidden md:flex items-start px-4 py-2 mx-auto max-w-[500px]">
+        <div className='text-white/70'>
+            <div className="hidden md:flex items-start px-4 py-2 mx-auto max-w-xl ">
                 <ul className='flex flex-row p-4 space-x-8'>
                     {
                         navLinks.map((link, index) => (
@@ -57,7 +57,7 @@ const Navbar = () => {
                     }
                     <li>
                         <a href="#contact" className='group'>
-                            <h1 className='text-lg font-bold text-white/70 cursor-pointer'>Contact Me</h1>
+                            <h1 className='text-lg font-semibold text-white/70 cursor-pointer'>Contact Me</h1>
                             <div className='relative'>
                                 <div className="absolute w-2/3 h-1 transition-all duration-300 ease-out
                             bg-[#6EF3FF] rounded-full group-hover:w-full
